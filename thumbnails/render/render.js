ee897b035e6da0caca10110acd7091c21c71cef7
Jonathas Barbosa <jnthas@gmail.com>
2026-02-10 16:47:05 -0300

    fix thumbnail renderer

--- thumbnails/render/render.js
+++ thumbnails/render/render.js
@@ -5,24 +5,26 @@ import * as url from 'url';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-
-
-
-
 (async () => {
 
   const directoryPath = path.join(__dirname, 'shared');
-  const files = fs.readdirSync(directoryPath);
+  let files = [];
+
+  if (process.argv.length > 2) {
+    files = process.argv.slice(2);
+  } else {
+    files = fs.readdirSync(directoryPath);
+  }
 
   const browser = await puppeteer.launch({
     args: [`--no-sandbox`, `--headless`, `--disable-gpu`, `--disable-dev-shm-usage`]
   });
-      
+
   const page = await browser.newPage();
   await page.setViewport({
-      width: 128,
-      height: 128,
-      deviceScaleFactor: 1,
+    width: 128,
+    height: 128,
+    deviceScaleFactor: 1,
   });
 
 
@@ -30,15 +32,15 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
     console.log(file);
 
-    const cw = path.parse(file).name  
+    const cw = path.parse(file).name
 
     await page.goto(`https://editor.clockwise.page/thumbnail.html?cw=${cw}`, { waitUntil: 'networkidle2' });
-    
-    await page.screenshot({path: `exported/${cw}.png`});
+
+    await page.screenshot({ path: `exported/${cw}.png` });
 
   };
 
-  
+
   await browser.close();
 
   console.log('End of execution');
